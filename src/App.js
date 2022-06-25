6f06fdd73cb66575cc2f25257a64515efbca8353
minh0518 <rubics@naver.com>
2022-06-25 21:22:03 +0900

    카테고리 적용

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
-import { useEffect, useState } from 'react'
+import { useCallback, useEffect, useState } from 'react'
 import axios from 'axios'
 import NewsList from './components/NewsList'
+import Categories from './components/Categories'
 
 const App = () => {
 
-   return(
-    <NewsList />
-   )
 
+   const [category,setCategory]=useState('all') //카테고리 디폴트는 all
+   const onSelect=useCallback(category=>setCategory((category),[]))
+
+  return (
+    <>
+      {/* 카테고리 상태와 카테고리 변경 함수를 넘겨줌 */}
+      <Categories category={category} onSelect={onSelect}/>
+      <NewsList category={category}/>
+    </>
+  )
 }
 export default App
--- src/components/NewsList.js
+++ src/components/NewsList.js
@@ -16,14 +16,8 @@ const NewsItemBlock = styled.div`
     }
 `
 
-// const sampleArticle={
-//     title : '제목',
-//     description : '내용',
-//     url : 'https://google.com',
-//     urlToImage : 'https://via.placeholder.com/160'
-// }
 
-const NewsList = () => {
+const NewsList = ({category}) => { //App.js에서 넘겨준 porps 사용
 
     const [articles,setArticles]=useState(null)
     const [loading,setLoadging]=useState(false)
@@ -34,7 +28,14 @@ const NewsList = () => {
         const fetchData= async ()=>{
             setLoadging(true)
             try{
-                const response=await axios.get('https://newsapi.org/v2/top-headlines?country=kr&apiKey=a8dab6844dca4de7b2800aa0312b9d54')
+
+                //넘겨받은 카테고리가 all이면 공백,
+                //아니라면 이 값으로 쿼리스트링을 이용
+                const query= category==='all'? ``:`&category=${category}`
+
+                const response=await axios.get(`https://newsapi.org/v2/top-headlines?country=kr${query}&apiKey=a8dab6844dca4de7b2800aa0312b9d54`)
+                //카테고리에 따른 API를 불러옴
+
                 setArticles(response.data.articles)
             }
             catch(e){
@@ -44,7 +45,8 @@ const NewsList = () => {
         }
         fetchData() //실행
 
-    },[])
+    },[category])
+    //category가 변할 때마다 뉴스를 새로 불러와야 하기 때문에 의존성 배열 사용
 
     //대기 중일 때
     if(loading){
@@ -54,9 +56,6 @@ const NewsList = () => {
     if(!articles){
         return null
     }
-
-
-
     
     return(
         <NewsItemBlock>
